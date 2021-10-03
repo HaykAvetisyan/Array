be10b8644a4604e5483408228eb3b55c9330cee1
Hayk Avetisyan <haykavetisyan992@gmail.com>
2021-10-03 14:43:23 +0400

    add Main, Example

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+       /* int m = 71;
+
+        if (m > 4) {
+            System.out.println(",e");
+        } else {
+            System.out.println("ada");
+        }
+        */
+
+
+
+
+
+
+
+        for (char ch1='ա'; ch1<='ֆ'; ch1++) {
+            System.out.println(ch1);
+        }
+
+    }
+}
